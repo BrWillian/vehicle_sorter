8cead2e88f06e1d6422050650e286cff2455b3f3
Willian <wiliam-m-@hotmail.com>
2020-01-21 16:25:31 -0300

    Features

--- main.py
+++ main.py
@@ -10,47 +10,52 @@ import time
 import cv2
 import os
 
-
 dir_train = '/kaggle/input/iara-sixs/modeldata/train'
 dir_val = '/kaggle/input/iara-sixs/modeldata/valid'
 dir_test = '/kaggle/input/iara-sixs/modeldata/test'
 
-IMG_SHAPE = (150,150, 3)
+IMG_SHAPE = (128, 128, 3)
 VGG19_MODEL = VGG19(input_shape=IMG_SHAPE, include_top=False, weights='imagenet')
 
-VGG19_MODEL.trainable = False
-global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+# VGG19_MODEL.trainable = True
+print(len(VGG19_MODEL.layers))
+
+for layer in VGG19_MODEL.layers[:-4]:
+    layer.trainable = False
+
+for layer in VGG19_MODEL.layers:
+    print(layer, layer.trainable)
+
 prediction_layer = tf.keras.layers.Dense(6, activation='softmax')
 
 model = Sequential([
     VGG19_MODEL,
-    global_average_layer,
-    tf.keras.layers.Dense(2048, activation='relu'),
-    tf.keras.layers.Dense(2048, activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(4096, activation='relu'),
+    tf.keras.layers.Dense(4096, activation='relu'),
     prediction_layer
 ])
 
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     dir_train,
-    target_size=(150, 150),
+    target_size=(128, 128),
     class_mode='categorical',
     batch_size=100
 )
 
 valid_generator = test_datagen.flow_from_directory(
     directory=dir_val,
-    target_size=(150, 150),
+    target_size=(128, 128),
     class_mode='categorical',
     batch_size=100
 )
 test_generator = test_datagen.flow_from_directory(
     directory=dir_test,
-    target_size=(150, 150),
+    target_size=(128, 128),
     class_mode='categorical',
     batch_size=100
 )
@@ -59,33 +64,27 @@ for data_batch, label_batch in train_generator:
     print(data_batch.shape, label_batch.shape)
     break
 
+callback = tf.keras.callbacks.ModelCheckpoint('/kaggle/working/bestmodel1.hdf5', monitor='val_loss',
+                                              save_best_only=True, verbose=1, mode='auto')
+
+Adam = tf.keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=Adam,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.summary()
-
-callback = tf.keras.callbacks.ModelCheckpoint('/kaggle/working/bestmodel1.hdf5', monitor='val_loss', save_best_only=True, verbose=1, mode='auto')
-
-Adam = tf.keras.optimizers.Adam(learning_rate=0.00001)
-model.compile(optimizer=Adam,
-               loss='categorical_crossentropy',
-               metrics=['accuracy'])
-
 
 train_size = train_generator.n
 valid_size = valid_generator.n
 test_size = test_generator.n
 
-history = model.fit_generator(train_generator,
-                              steps_per_epoch=train_size/100,
-                              epochs=100,
-                              validation_data=valid_generator,
-                              validation_steps=valid_size/100, callbacks=[callback])
+history = model.fit_generator(train_generator, steps_per_epoch=train_size / 100, epochs=200,
+                              validation_data=valid_generator, validation_steps=valid_size / 100, callbacks=[callback])
 
-
-model_save = load_model('bestmodel1.hdf5')
+model_save = load_model('/kaggle/working/bestmodel1.hdf5')
 
 loss, result = model_save.evaluate_generator(test_generator)
 print(result, loss)
-
 
 
 plt.plot(history.history['accuracy'])
