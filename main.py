3d93e7b43bd2bd4eabf3ad7977740e68461df63f
Willian <wiliam-m-@hotmail.com>
2020-02-12 14:28:53 -0300

    Fixed Model ~98.5%

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ import time
 import cv2
 import os
 
-dir_train = '/kaggle/input/vehicle-sorter-v2/modeldata/train'
-dir_val = '/kaggle/input/vehicle-sorter-v2/modeldata/valid'
+#Set Directory
+dir_train = 'modeldata/train'
+dir_val = 'modeldata/valid'
 
 IMG_SHAPE = (300, 300, 3)
 VGG19_MODEL = VGG19(input_shape=IMG_SHAPE, include_top=False, weights='imagenet')
 
+#Traing weights VGG19
 VGG19_MODEL.trainable = True
 print(len(VGG19_MODEL.layers))
 
@@ -24,6 +26,7 @@ for layer in VGG19_MODEL.layers:
 
 prediction_layer = tf.keras.layers.Dense(6, activation='softmax')
 
+#Create model
 model = Sequential([
     VGG19_MODEL,
     tf.keras.layers.Flatten(),
@@ -33,6 +36,7 @@ model = Sequential([
     prediction_layer
 ])
 
+#Set DataGenerator
 train_datagen = ImageDataGenerator(rescale=1. / 255)
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -51,13 +55,16 @@ valid_generator = test_datagen.flow_from_directory(
     batch_size=32
 )
 
+#Print shape of datagenerator
 for data_batch, label_batch in train_generator:
     print(data_batch.shape, label_batch.shape)
     break
 
-callback = tf.keras.callbacks.ModelCheckpoint('/kaggle/working/model.hdf5', save_best_only=True, monitor='val_accuracy',
+#Create checkpoint and save best model
+callback = tf.keras.callbacks.ModelCheckpoint('model.hdf5', save_best_only=True, monitor='val_accuracy',
                                               verbose=1, mode='auto')
 
+#Configure Optimizers
 Adam = tf.keras.optimizers.Adam(learning_rate=0.00001)
 model.compile(optimizer=Adam,
               loss='categorical_crossentropy',
@@ -68,4 +75,5 @@ model.summary()
 train_size = train_generator.n
 valid_size = valid_generator.n
 
+#Train model
 history = model.fit_generator(train_generator, epochs=50, validation_data=valid_generator, callbacks=[callback])
