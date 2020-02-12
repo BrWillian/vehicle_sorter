83f9555e54d7f2ba9dece70f288f77bd8b2292c0
Willian <wiliam-m-@hotmail.com>
2020-02-12 14:31:22 -0300

    Fixed Augmentation

--- augmentation.py
+++ augmentation.py
@@ -19,7 +19,7 @@ def generate_images(dir):
     for root, _, files in os.walk(dir):
         for f in files:
             img = load_img(root+f)
-            x = img_to_array(img)  # transforma imagem em array
+            x = img_to_array(img)  #transforma imagem em array
             x = x.reshape((1,) + x.shape)
             i = 0
             for batch in datagen.flow(x, batch_size=1, save_to_dir='images_data_augmentation', save_prefix='aug', save_format='jpg'):
